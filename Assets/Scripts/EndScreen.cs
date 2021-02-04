1fc464822c990d60e6244a2af88719777f92fcbf
ketrab2004 <bartek@fos.nl>
2021-02-04 12:01:47 +0100

    Made end screen script for end screen

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScreen : MonoBehaviour
+{
+    public void ShowEndScreen()
+    {
+        
+    }
+
+    public void HideEndScreen()
+    {
+        
+    }
+}
