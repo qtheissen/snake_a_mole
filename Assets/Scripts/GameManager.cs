bc9abab89709042f35a00720b6ceb12681874f28
ketrab2004 <bartek@fos.nl>
2021-02-05 10:25:14 +0100

    Changed so that timer stops counting down when game over Changed shows end screen when game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,12 @@ public class GameManager : MonoBehaviour
     private TMP_Text scoreCounterTMP;
     private TMP_Text timerTMP;
 
+    [HideInInspector] // Bool to keep track of game over (not to be edited in inspector)
+    public bool gameOver = false;
+    
+    // For end screen animation
+    public EndScreen endScreenUI;
+
     // This is the total time left in seconds for the game
     public float totalTimeSeconds = 100f;
 
@@ -36,7 +42,18 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         Timer();
-        UpdateTimer();
+        if (!gameOver) // Only update timer when game is not over
+        {
+            UpdateTimer();
+        }
+
+        if (totalTimeSeconds < 0 && !gameOver) // Dont game over if already game over
+        {
+            gameOver = true;
+            endScreenUI.ShowEndScreen(score); // Game over ui
+
+            totalTimeSeconds = 0; // Set timer to 0 so it doesn't go negative
+        }
     }
 
     void Timer()
